91a86d90aa466db352db528434a631b84c0a7bfb
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-28 08:58:57 -0700

    Update stream.php

--- stream.php
+++ stream.php
@@ -18,7 +18,7 @@ if (!empty($existpid)) {
 // Start new stream
 $command = "/usr/bin/nohup /var/www/html/yt-dlp_linux https://www.youtube.com/watch?v=$safe_idstream -o - " .
     "| ffmpeg -re -i - -acodec amr_wb -ar 16000 -ac 1 -ab 24k -vcodec mpeg4 -vb 104k -r 15 -vf scale=320:240 " .
-    "-f rtsp rtsp://127.0.0.1:8080/$idstream >/tmp/yt_dlpdebug.txt 2>&1 &";
+    "-f rtsp rtsp://127.0.0.1:554/$idstream >/tmp/yt_dlpdebug.txt 2>&1 &";
 exec($command);
 
 // Wait and check if process started
@@ -28,7 +28,7 @@ if (!$newpid) {
     die("Failed to start streaming.");
 }
 
-// Output stream link (only port 8000)
-echo "<a href='rtsp://enthusiastic-edeline-kmsepr-0cbdd0dd.koyeb.app:8000/$idstream'>Watch</a> *Port 8000<br>";
+// Output stream link (only port 554)
+echo "<a href='rtsp://enthusiastic-edeline-kmsepr-0cbdd0dd.koyeb.app:554/$idstream'>Watch</a> *Port 554<br>";
 echo "<br><a href='index.php'>Back</a>";
 ?>
