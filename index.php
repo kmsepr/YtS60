a4432b2725fd9aac0b72cf9156ebbea2a86ed9e8
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-28 10:42:53 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,26 @@
 <?php
-echo "<h1>YouTube Video Search</h1>";
+$api_key = getenv('YOUTUBE_API_KEY') ?: 'YOUR_YOUTUBE_API_KEY';
+$query = $_GET['query'] ?? '';
 
-echo "<form action=\"index.php\" method=\"POST\">
-    YouTube Search: <input type=\"text\" name=\"videoname\">
-    <input type=\"submit\" value=\"Search\">
-</form>";
-
-$api_key = getenv('YOUTUBE_API_KEY'); // Ensure this is set in your environment
-$search_query = $_POST["videoname"] ?? '';
-
-if (!empty($search_query)) {
-    $encoded_query = urlencode($search_query);
-    $search_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=5&q=$encoded_query&key=$api_key";
-
-    $response = file_get_contents($search_url);
-    $data = json_decode($response, true);
-
-    if (!isset($data['items'])) {
-        die("Error fetching search results.");
-    }
-
-    foreach ($data['items'] as $item) {
-        $video_id = $item['id']['videoId'];
-        $title = htmlspecialchars($item['snippet']['title'], ENT_QUOTES, 'UTF-8');
-        $thumbnail = $item['snippet']['thumbnails']['medium']['url'];
-
-        echo "<div>
-            <a href='download.php?id=$video_id'><img src='$thumbnail' alt='$title'></a><br>
-            <a href='download.php?id=$video_id'>$title</a><br>
-        </div><br>";
-    }
+if ($query) {
+    $search_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" . urlencode($query) . "&type=video&key=$api_key&maxResults=5";
+    $response = json_decode(file_get_contents($search_url), true);
 }
 ?>
+
+<form method="GET">
+    <input type="text" name="query" placeholder="Search YouTube" value="<?= htmlspecialchars($query) ?>">
+    <button type="submit">Search</button>
+</form>
+
+<?php if (!empty($response['items'])): ?>
+    <ul>
+        <?php foreach ($response['items'] as $video): ?>
+            <li>
+                <a href="stream.php?id=<?= $video['id']['videoId'] ?>" target="_blank">
+                    <?= htmlspecialchars($video['snippet']['title']) ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
