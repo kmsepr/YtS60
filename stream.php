605c51d0550b3cb64bc67fb3f954d13661a753e0
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-28 22:56:19 -0700

    Update stream.php

--- stream.php
+++ stream.php
@@ -14,9 +14,10 @@ if (!empty($existpid)) {
     exec("kill $existpid");
 }
 
-// Start RTSP Streaming Process
+// Start RTSP Streaming Process with 240p Low Bitrate
 $command = "/usr/bin/nohup yt-dlp -f 'best[ext=mp4]' https://www.youtube.com/watch?v=$safe_idstream -o - | " .
-    "ffmpeg -re -i pipe:0 -c:v libx264 -preset ultrafast -crf 18 -c:a aac -b:a 128k " .
+    "ffmpeg -re -i pipe:0 -c:v libx264 -preset ultrafast -b:v 250k -maxrate 300k -bufsize 500k -r 15 -vf scale=320:240 " . // Video: 250kbps, 15fps, 320x240
+    "-c:a aac -b:a 32k -ar 22050 " . // Audio: 32kbps, 22.05kHz
     "-f rtsp rtsp://tv.tg-gw.com/$safe_idstream " .
     ">/tmp/yt_dlpdebug.txt 2>&1 &";
 
@@ -31,6 +32,6 @@ if (!$newpid) {
 
 // Display RTSP Stream Links
 echo "<h3>Stream Started</h3>";
-echo "<a href='rtsp://tv.tg-gw.com:8080/$safe_idstream'>RTSP Link 1 (Port 80)</a><br>";
+echo "<a href='rtsp://tv.tg-gw.com:8080/$safe_idstream'>RTSP Link 1 (Port 8080)</a><br>";
 
 ?>
